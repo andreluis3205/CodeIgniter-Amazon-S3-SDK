38acacfef1bb532affb0188af2e0164ddcde3b73
Ahmad Gneady <github@bigprof.com>
2013-06-28 19:33:11 +0200

    Corrected file encoding to UTF-8 without BOM

--- libraries/s3.php
+++ libraries/s3.php
@@ -2,16 +2,16 @@
 /**
 * $Id$
 *
-* Copyright (c) 2011, Donovan Sch?nknecht.  All rights reserved.
+* Copyright (c) 2013, Donovan Schönknecht. All rights reserved.
 *
 * Redistribution and use in source and binary forms, with or without
 * modification, are permitted provided that the following conditions are met:
 *
 * - Redistributions of source code must retain the above copyright notice,
-*   this list of conditions and the following disclaimer.
+* this list of conditions and the following disclaimer.
 * - Redistributions in binary form must reproduce the above copyright
-*   notice, this list of conditions and the following disclaimer in the
-*   documentation and/or other materials provided with the distribution.
+* notice, this list of conditions and the following disclaimer in the
+* documentation and/or other materials provided with the distribution.
 *
 * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
@@ -27,6 +27,7 @@
 *
 * Amazon S3 is a trademark of Amazon.com, Inc. or its affiliates.
 */
+
 
 /**
 * Amazon S3 PHP class
